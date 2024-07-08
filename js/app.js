738b28190a6dd5f6c67f0097063887848883632e
Florencia <florenciamorelliIT@gmail.com>
2024-07-08 16:19:37 -0300

    functioning app

---
+++ js/app.js
@@ -0,0 +1,31 @@
+const selectPropiedad = document.getElementById('propiedad');
+const selectUbicacion = document.getElementById('ubicacion');
+const inputMetros2 = document.getElementById('metros2');
+const btnCotizar = document.getElementById('cotizar');
+const spanPoliza = document.getElementById('valorPoliza');
+
+btnCotizar.addEventListener('click', () => {
+    const propiedad = selectPropiedad.value;
+    const ubicacion = selectUbicacion.value;
+    const metros2 = inputMetros2.value;
+
+    if(propiedad === null){
+        alert('Seleccione una propiedad');
+    } else if (ubicacion === null){
+        alert('Seleccione una ubicación');
+    } else if (metros2 <= 0){
+        alert('Ingrese un valor mayor a 0');
+    } else {
+        const valorPoliza = calcularPoliza(propiedad, ubicacion, metros2);
+        spanPoliza.innerText = valorPoliza.toFixed(2);
+    }
+})
+
+function calcularPoliza(propiedad, ubicacion, metros2){
+    let valorPoliza = 0;
+        
+    valorPoliza = propiedad * ubicacion * metros2;
+
+    return valorPoliza;
+
+}
