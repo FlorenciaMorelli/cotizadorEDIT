8d24f5f00b8da1b405bd1567d8eed65dca727a3c
Florencia <florenciamorelliIT@gmail.com>
2024-07-08 16:27:59 -0300

    fixed null value issues

--- js/app.js
+++ js/app.js
@@ -11,10 +11,13 @@ btnCotizar.addEventListener('click', () => {
 
     if(propiedad === null){
         alert('Seleccione una propiedad');
+        spanPoliza.innerText = "";
     } else if (ubicacion === null){
         alert('Seleccione una ubicación');
+        spanPoliza.innerText = "";
     } else if (metros2 <= 0){
         alert('Ingrese un valor mayor a 0');
+        spanPoliza.innerText = "";
     } else {
         const valorPoliza = calcularPoliza(propiedad, ubicacion, metros2);
         spanPoliza.innerText = valorPoliza.toFixed(2);
